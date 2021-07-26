dca123b85078fbe8d5a5f5565383a1453ccfd447
teerawat prarom <teewat@yahoo.com>
2021-07-27 00:37:32 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector('main');
+main.remove();
+const newHeader = document.createElement('H1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = '"Tee is the champion"';
+document.body.appendChild(newHeader);
